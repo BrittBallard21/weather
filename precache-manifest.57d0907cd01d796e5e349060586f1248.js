9c7a57141203582d47f652638d25d62e75a7821c
brittany <bball24x7@gmail.com>
2020-02-26 19:51:37 -0800

    Updates

---
+++ precache-manifest.57d0907cd01d796e5e349060586f1248.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d268d0750bb53aecc5284b51c7c693b",
+    "url": "/weather/index.html"
+  },
+  {
+    "revision": "2fc01b6d015850f742a0",
+    "url": "/weather/static/css/main.f41929ee.chunk.css"
+  },
+  {
+    "revision": "ea34ff7a1bf6576f7bff",
+    "url": "/weather/static/js/2.5204da3b.chunk.js"
+  },
+  {
+    "revision": "2fc01b6d015850f742a0",
+    "url": "/weather/static/js/main.3b113b18.chunk.js"
+  },
+  {
+    "revision": "a1608adf0ad63e4fd58d",
+    "url": "/weather/static/js/runtime-main.de26c817.js"
+  },
+  {
+    "revision": "176637e3c6f8f35e7a3662a8896d8f19",
+    "url": "/weather/static/media/cloudy.176637e3.png"
+  },
+  {
+    "revision": "d10b6c70766463b300d8d3e43aab104d",
+    "url": "/weather/static/media/rain.d10b6c70.png"
+  },
+  {
+    "revision": "ba186b3fc8a4ff38cb550198efb81332",
+    "url": "/weather/static/media/snowflake.ba186b3f.png"
+  },
+  {
+    "revision": "bcb3306a08b8a1c94207d7b0c2113acf",
+    "url": "/weather/static/media/sun.bcb3306a.png"
+  },
+  {
+    "revision": "f6718ab117748160bf30a287c81569d0",
+    "url": "/weather/static/media/wind.f6718ab1.png"
+  }
+]);
